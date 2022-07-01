a09a4c2a376be7e0d320b3cc73f8ee45934b5010
patipan-patisampita <patipan.patisampita@gmail.com>
2022-07-01 16:36:47 +0700

    update

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'hello.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key); //1.constructor
+
+  @override //2.build
+  Widget build(BuildContext context) {
+     // int _count = 0;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HelloPage(),
+    );
+  }
+}
+
+
+
+
